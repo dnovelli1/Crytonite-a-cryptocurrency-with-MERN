f4385201623d3ac6e080e4be72f70ff5397f40c5
Jake Novelli <jakenovelli11@gmail.com>
2021-06-06 18:17:34 -0700

    added homestyling

--- client/src/components/LearnMore/index.js
+++ client/src/components/LearnMore/index.js
@@ -5,15 +5,14 @@ import "./style.css";
 
 const title = "Learn More";
 
-const body = (
-  <Row>
-    <Col md={6}><img src={'https://cdn.pixabay.com/photo/2018/01/17/20/22/analytics-3088958_960_720.jpg'} className="image" alt='Learn More' /></Col>
-    <Col md={6} className="cardBody" id="learnCard"> <h1>{title}</h1>
-      <p>Cryptocurrency -- aka crypto -- is a digital currency designed to work as a medium of exchange. It uses cryptography (the practice of securing communication under a third party) to secure and verify transactions, as well as to control the creation of new units of a particular cryptocurrency. The best feature of a cryptocurrency is the fact that it is not controlled by any central authority. The decentralized nature of the  blockchain makes cryptocurrency theoretically immune to the old ways of government control and interference. Cryptocurrencies can be traded between two parties using public and private keys.</p>
-    </Col>
-
-  </Row>
-);
+// const body = (
+//   // <Row>
+//   //   <Col md={5}><img style={{maxHeight: 400}} src={'https://cdn.pixabay.com/photo/2018/01/17/20/22/analytics-3088958_960_720.jpg'} className="image" alt='Learn More'></img></Col>
+//   //   <Col md={7} className="cardBody" id="learnCard"> <h1>{title}</h1>
+//   //     <p>Cryptocurrency -- aka crypto -- is a digital currency designed to work as a medium of exchange. It uses cryptography (the practice of securing communication under a third party) to secure and verify transactions, as well as to control the creation of new units of a particular cryptocurrency. The best feature of a cryptocurrency is the fact that it is not controlled by any central authority. The decentralized nature of the  blockchain makes cryptocurrency theoretically immune to the old ways of government control and interference. Cryptocurrencies can be traded between two parties using public and private keys.</p>
+//   //   </Col>
+//   // </Row>
+// );
 
 function LearnMore() {
   if (isMobile) {
@@ -25,7 +24,12 @@ function LearnMore() {
 
         <Accordion.Collapse eventKey="1">
           <Card.Body>
-            {body}
+            <Row>
+              <Col md={5}><img style={{ maxHeight: 400, maxWidth: 500 }} src={'https://cdn.pixabay.com/photo/2018/01/17/20/22/analytics-3088958_960_720.jpg'} className="image" alt='Learn More'></img></Col>
+              <Col md={7} className="cardBody" id="learnCard"> <h1>{title}</h1>
+                <p>Cryptocurrency -- aka crypto -- is a digital currency designed to work as a medium of exchange. It uses cryptography (the practice of securing communication under a third party) to secure and verify transactions, as well as to control the creation of new units of a particular cryptocurrency. The best feature of a cryptocurrency is the fact that it is not controlled by any central authority. The decentralized nature of the  blockchain makes cryptocurrency theoretically immune to the old ways of government control and interference. Cryptocurrencies can be traded between two parties using public and private keys.</p>
+              </Col>
+            </Row>
           </Card.Body>
         </Accordion.Collapse>
 
@@ -34,9 +38,14 @@ function LearnMore() {
   } else {
     return (
       <Card className="desktopCard">
-          <Card.Body>
-            {body}
-          </Card.Body>
+        <Card.Body>
+          <Row>
+            <Col md={5}><img style={{ maxHeight: 400 }} src={'https://cdn.pixabay.com/photo/2018/01/17/20/22/analytics-3088958_960_720.jpg'} className="image" alt='Learn More'></img></Col>
+            <Col md={7} className="cardBody" id="learnCard"> <h1>{title}</h1>
+              <p>Cryptocurrency -- aka crypto -- is a digital currency designed to work as a medium of exchange. It uses cryptography (the practice of securing communication under a third party) to secure and verify transactions, as well as to control the creation of new units of a particular cryptocurrency. The best feature of a cryptocurrency is the fact that it is not controlled by any central authority. The decentralized nature of the  blockchain makes cryptocurrency theoretically immune to the old ways of government control and interference. Cryptocurrencies can be traded between two parties using public and private keys.</p>
+            </Col>
+          </Row>
+        </Card.Body>
       </Card>
     )
   }
